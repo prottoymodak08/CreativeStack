5502c92e7da7c99e611ec1092764372188c5496c
Prottoy Modak <116016410+prottoymodak08@users.noreply.github.com>
2025-05-23 17:45:46 +0600

    JS TypeWriter Effect

--- js/script.js
+++ js/script.js
@@ -23,5 +23,53 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 50);
   });
 
-  
+  //============================================
+  // TypeWriter Effect
+  // ===========================================
+  const typeWriterWords = [
+    "Fontend Developer",
+    "SEO Enthusiast",
+    "Web Designer",
+    "UI/UX Designer",
+  ];
+
+  let typewriterSpan = document.querySelector(".typewriter_effect");
+
+  let currentWordIndex = 0;
+  let currentCharIndex = 0;
+  let isDeleting = false;
+
+  function typewriterEffect() {
+    let currentWord = typeWriterWords[currentWordIndex];
+    let displayWord;
+
+    if (!isDeleting) {
+      currentCharIndex++;
+      displayWord = currentWord.substring(0, currentCharIndex);
+    } else {
+      currentCharIndex--;
+      displayWord = currentWord.substring(0, currentCharIndex);
+    }
+
+    typewriterSpan.textContent = displayWord;
+
+    let typeWriterEffectDelay = 100;
+
+    if (isDeleting) {
+      typeWriterEffectDelay = 50;
+    }
+
+    if (!isDeleting && currentCharIndex == currentWord.length) {
+      typeWriterEffectDelay = 1000;
+      isDeleting = true;
+    } else if (isDeleting && currentCharIndex == 0) {
+      typeWriterEffectDelay = 200;
+      isDeleting = false;
+      currentWordIndex = (currentWordIndex + 1) % typeWriterWords.length;
+    }
+
+    setTimeout(typewriterEffect, typeWriterEffectDelay);
+  }
+
+  typewriterEffect();
 });
